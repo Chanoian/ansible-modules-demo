7c8a63d0b689e083e6c0a0d2f8a0d35f0ff45db4
Ara Chanoian <builder@DESKTOP-ABRS4KI.localdomain>
2021-10-29 19:56:57 -0400

    add the doc to the module

--- library/ansible_modules_counter.py
+++ library/ansible_modules_counter.py
@@ -6,30 +6,49 @@ from ansible.module_utils.basic import AnsibleModule
 
 MODULES_URL = "https://docs.ansible.com/ansible/2.8/modules/"
 
+DOCUMENTATION = '''
+---
+module: ansible_modules_countere
+short_description: This module will return the number of available modules for specific module type
+author:
+  - Ara Chanoian
+'''
 
-
+EXAMPLES = '''
+- name: Count the number of modules available for cloud
+  ansible_modules_counter:
+    modules_type: cloud
+'''
 
 def main():
     # define the parameters the user needs to pass to the module
     args = dict(
-        module_type=dict(type='str', required=True))
-    module = AnsibleModule(
-        argument_spec=args,
-        supports_check_mode=True
-    )
-    module_type = module.params['module_type']
-    url = MODULES_URL + 'list_of_{}_modules.html'.format(module_type)
+        modules_type=dict(type='str', required=True))
+    module = AnsibleModule(argument_spec=args)
+    modules_type = module.params['modules_type']
+    url = MODULES_URL + 'list_of_{}_modules.html'.format(modules_type)
     try:
-        return_req = requests.get(url=url)
-        soup = BeautifulSoup(return_req.content, 'html.parser')
+        response = requests.get(url=url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
         counter = 0
         for ultag in soup.find_all('ul', class_='simple'):
             for litag in ultag.find_all('li'):
                 counter+=1
-        module.exit_json(changed = True, module_counter=counter)
+        # remove the note from the count
+        counter = counter - 1
+        module.exit_json(changed = False, module_counter=counter)
     except Exception as e:
-        module.fail_json(msg = e)
+        module.fail_json(msg = "Make Sure The Modules Type {} is Correct !".format(modules_type))
 
 
 if __name__ == '__main__':
     main()
+
+
+#  args=dict(
+#             name=dict(type='str', required=True),
+#             sleep=dict(type='int', required=False),
+#             enabled=dict(type='bool', default=True),
+#             api_token=dict(type='str', no_log=True),
+#             state=dict(type='str', choices=['started', 'stopped', 'reloaded', 'restarted']))
